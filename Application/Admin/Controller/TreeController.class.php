0c80145808e3034ab51a22bfcda94a454cb498de
fuermolv <302889601@qq.com>
2017-07-20 23:09:52 +0800

    修改主界面
    修改主界面

---
+++ Application/Admin/Controller/TreeController.class.php
@@ -0,0 +1,28 @@
+<?php
+namespace Admin\Controller;
+use Common\Controller\PublicBaseController;
+/**
+ * 后台首页控制器
+ */
+class TreeController extends PublicBaseController{
+	/**
+	 * 首页
+	 */
+	public function index(){
+		$this->display();
+	}
+	
+	
+	/**
+	 * welcome
+	 */
+	public function group($name='thinkphp')
+	{
+	    
+	     $this->assign('name',$name);
+        $this->display();
+	}
+
+
+
+}
--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -26,13 +26,13 @@ return array(
         '__APP_IMAGES__'     => OSS_URL.trim(TMPL_PATH,'.').'App/Public/images'
     ),
 //***********************************URL设置**************************************
-    'MODULE_ALLOW_LIST'      => array('Home','Admin','Api','User','App'), //允许访问列表
+    'MODULE_ALLOW_LIST'      => array('Home','Admin','Api','User','App','Tree'), //允许访问列表
     'URL_HTML_SUFFIX'        => '',  // URL伪静态后缀设置
     'URL_MODEL'              => 1,  //启用rewrite
 //***********************************SESSION设置**********************************
     'SESSION_OPTIONS'        => array(
-        'name'               => 'BJYADMIN',//设置session名
-        'expire'             => 24*3600*15, //SESSION保存15天
+        'name'               => 'TreeSession',//设置session名
+        'expire'             => 24*3600*7, //SESSION保存15天
         'use_trans_sid'      => 1,//跨页传递
         'use_only_cookies'   => 0,//是否只开启基于cookies的session的会话方式
     ),
