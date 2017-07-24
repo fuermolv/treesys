15e0c82d9aeb3ef5e1a51c643960521ca24b55d4
fuermolv <302889601@qq.com>
2017-07-24 23:59:42 +0800

    添加基本列表页
    添加基本列表页

--- Application/Admin/Controller/LineController.class.php
+++ Application/Admin/Controller/LineController.class.php
@@ -1,6 +1,7 @@
 <?php
 namespace Admin\Controller;
 use Common\Controller\AdminBaseController;
+use  Common\Model\TreeBaseModel;
 /**
  * 后台首页控制器
  */
@@ -8,10 +9,20 @@ class LineController extends AdminBaseController{
 	/**
 	 * 首页
 	 */
-	public function index($id='thinkphp')
-	{
-		 $this->assign('name',$name);
-		$this->display();
+	public function index($id='thinkphp'){
+         
+           $map=array(
+            'line_id'=>$id
+            );
+                
+		 $data=D('TreeBase')->getPage(new TreeBaseModel(),$map);
+
+		
+
+		 var_dump($data);
+		 $this->assign('data',$data['data']);
+		 $this->assign('pagehtml',$data['page']);
+		 $this->display();
 	}
 	
 	
