52558162fc7c06e05dbd2cf0fdca4dddf4556aa5
fuermolv <302889601@qq.com>
2017-07-25 23:50:46 +0800

    1
    1

--- Application/Admin/Controller/LineController.class.php
+++ Application/Admin/Controller/LineController.class.php
@@ -1,25 +1,45 @@
 <?php
 namespace Admin\Controller;
-use Common\Controller\AdminBaseController;
+use Common\Controller\TreeSysController;
 use  Common\Model\TreeBaseModel;
+
 /**
  * 后台首页控制器
  */
-class LineController extends AdminBaseController{
+class LineController extends TreeSysController{
 	/**
 	 * 首页
 	 */
 	public function index($id='thinkphp'){
          
-           $map=array(
-            'line_id'=>$id
-            );
+           $map=array('line_id'=>$id);
+           $limit=25;
                 
-		 $data=D('TreeBase')->getPage(new TreeBaseModel(),$map);
+		// $data=D('TreeBase')->getPage(new TreeBaseModel(),$map,$order,$limit);
+         $model=new TreeBaseModel();
 
-		
+		 $count=$model
+            ->where($map)
+            ->count();
 
-		// var_dump($data);
+        $page=new_page($count,$limit);
+        $list=$model
+                ->field($field)
+                ->where($map)
+                ->alias('base')
+                ->join('__DEVICE_LINE__ dl ON base.line_id=dl.id','LEFT')
+                /*->order($order)*/
+                ->limit($page->firstRow.','.$page->listRows)
+                ->select();      
+                 $data=array(
+                'data'=>$list,
+                'page'=>$page->show()
+                 );
+   
+
+		 
+
+		 //var_dump($data);
 		 $this->assign('data',$data['data']);
 		 $this->assign('pagehtml',$data['page']);
 		 $this->display();
---
+++ Application/Common/Controller/TreeSysController.class.php
@@ -0,0 +1,25 @@
+<?php
+namespace Common\Controller;
+use Common\Controller\BaseController;
+/**
+ * admin 基类控制器
+ */
+class TreeSysController extends BaseController{
+	/**
+	 * 初始化方法
+	 */
+	public function _initialize(){
+		parent::_initialize();
+		$auth=new \Think\Auth();
+		$nav_data=D('AdminNav')->getTreeData('level','order_number,id');
+		$assign=array(
+			'nav_data'=>$nav_data
+			);
+		$this->assign($assign);
+	}
+
+
+
+
+}
+
