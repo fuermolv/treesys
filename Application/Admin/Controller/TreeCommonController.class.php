077efef434a00fa5a3641585f17fc7cada0d147f
fuermolv <302889601@qq.com>
2017-11-08 21:41:38 +0800

    1
    1

--- Application/Admin/Controller/TreeCommonController.class.php
+++ Application/Admin/Controller/TreeCommonController.class.php
@@ -38,6 +38,49 @@ class TreeCommonController extends PublicBaseController
         
          $this->ajaxReturn($data);
     }
+    public function freshForm(){
+        $edit_line_id = I('get.edit_line_id');
+        $edit_county = I('get.edit_county');
+        $edit_town = I('get.edit_town');
+        $edit_voltage_degree = I('get.edit_voltage_degree');
+        $edit_village = I('get.edit_village');
+        $edit_tid = I('get.edit_tid'); 
+        $group_id = I('get.group_id');
+                //确定班组线路       
+        $map=null;
+        $map['id'] = $group_id;
+        $edit_lienes = M("auth_rule")->where($map)->select();
+                
+        $map = null;
+        $map['did'] = array('in', $edit_lienes[0]['group_device']);
+        $edit_device_lines = M("device_line")->where($map)->select();
+        $querydata['device_lines'] = $edit_device_lines;
+                
+        $map = null;
+        //选出县镇乡
+        if (!empty($edit_county)) {
+            $map['fid'] = $edit_county;
+             $map['sid'] = 0;
+            $querydata['towns'] = M("areas")->where($map)->select();
+        }
+        $map = null;
+        if (!empty($edit_town)) {
+            $map['sid'] = $edit_town;
+            $querydata['villages'] = M("areas")->where($map)->select();
+        }
+        // var_dump($querydata); 
+        $this->assign('group_id', $group_id);
+        // $this->assign('edit_line_id', $edit_line_id);
+        // $this->assign('edit_county', $edit_county);
+        // $this->assign('edit_town', $edit_town);
+        // $this->assign('edit_village', $edit_village);
+        // $this->assign('edit_voltage_degree', $edit_voltage_degree);
+        $this->assign('edit_tid', $edit_tid);
+        $this->assign('querydata', $querydata);
+        
+        $content=$this->fetch();
+        $this->ajaxReturn($content);
+    } 
 
     
 }
