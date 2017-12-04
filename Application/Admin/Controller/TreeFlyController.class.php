28b7791e7c9b81d05e5a8eeb7043e6879611e070
lanfengchalanzhu <13450313141@163.com>
2017-12-05 01:02:06 +0800

    完善飞行记录的查看界面和新增数据界面
    目前已实现的有增删查，修改暂时还未做处理

--- Application/Admin/Controller/TreeFlyController.class.php
+++ Application/Admin/Controller/TreeFlyController.class.php
@@ -23,10 +23,12 @@ class TreeFlyController extends AdminBaseController
         $fly_plane_type=I('get.fly_plane_type');
         $model = new TreeFlyModel();
         $map['fly_tid'] = $tree_id;
-        if(empty($fly_plane_type))
+        if($fly_plane_type=="")
         {
-            $fly_plane_type=1;
+            $fly_plane_type="1";
         }
+        // var_dump($fly_plane_type);
+        
         $map['fly_plane_type'] = $fly_plane_type;
         $orderBy = 'fly_id desc';
         $data=$model->where($map)->order($orderBy)->select();
@@ -41,65 +43,60 @@ class TreeFlyController extends AdminBaseController
    public function delete() 
    {
         $fly_id = I('get.fly_id');
+        $fly_tid=I('get.fly_tid');
+        $group_id=I('get.group_id');
         $map['fly_id'] = $fly_id;
-        M("tree_fly")->where(array($map))->delete();
+        var_dump($fly_id);
+        $record=D('TreeFly');
+        $result = $record->where(array($map))->delete();
+        if($result){
+            $this->success("成功删除飞行记录",U("Admin/TreeFly/index/group_id/{$group_id}"));
+            }
+        else{   
+            $this->error('删除飞行记录失败');}
+        
    }
    public function add()
    {
-    $tree_id = I('get.tid');
-    $this->assign('tree_id',$tree_id);
-    $content=$this->fetch();
-    $this->ajaxReturn($content);
-    //    if(IS_POST)
-    //   {
+       if(IS_POST)
+      {
 
-    //       $ar=$_POST;
-    //       $fly_tid=$ar['fly_tid'];
-    //     //   $map['detail_tid']=$detail_tid;
-    //     //   $data['datail_uptodate']=0;
+          $ar=$_POST;
+          $fly_tid=$ar['fly_tid'];
+        //   $map['detail_tid']=$detail_tid;
+        //   $data['datail_uptodate']=0;
 
-    //     //   M("tree_detail")->where($map)->data($data)->save();
+        //   M("tree_detail")->where($map)->data($data)->save();
 
             
-    //       $map=null;
-    //       $user_id=$_SESSION['user']['id'];
+          $map=null;
+          $user_id=$_SESSION['user']['id'];
 
-    //       $map['id']=$user_id;
-    //       $user=M("users")->where($map)->select();
-    //       $ar['detail_last_time']=strtotime($ar['detail_last_time']);
-    //       $ar['datail_check_time']=strtotime($ar['datail_check_time']);
-    //       $ar['datail_update_time']=NOW_TIME;
-    //       $ar['datail_update_person']=$user[0]['true_name'];
-    //       $record=D('TreeDetail');
-    //       $record->addData($ar);
-
-
-    //       $basemap['tid']=$detail_tid;
-    //       $basedata['last_update_person']=$user[0]['true_name'];
-          
-    //       $basedata['last_update_time']=NOW_TIME;
-
-        
-
-    //       M("tree_base")->where($basemap)->save($basedata);
-
-
-
-    //       $this->ajaxReturn($detail_tid);
-    //   }
-    //   else
-    //   {
-    //       $tree_id = I('get.tid');
-    //       $map['fly_tid']=$tree_id;
-    //       $data=M("tree_fly")->where($map)->select();
-    //       $this->assign('data',$data[0]);
-    //       $this->assign('tree_id',$tree_id);
-    //       $content=$this->fetch();
-    //       var_dump($content);
-    //       $this->ajaxReturn($content);
+          $map['id']=$user_id;
+          $user=M("users")->where($map)->select();
+          $ar['fly_check_time']=strtotime($ar['fly_check_time']);
+          $ar['fly_start_time']=strtotime($ar['fly_start_time']);
+          $ar['fly_end_time']=strtotime($ar['fly_end_time']);
+          $ar['fly_report_made_time']=strtotime($ar['fly_report_made_time']);
+          $ar['fly_receive_report_time']=strtotime($ar['fly_receive_report_time']);
+          $ar['fly_group_receive_report_time']=strtotime($ar['fly_group_receive_report_time']);
+          $ar['fly_group_feedback_time']=strtotime($ar['fly_group_feedback_time']);
+          $ar['fly_later_deal_time']=strtotime($ar['fly_later_deal_time']); 
+          $ar['fly_update_time']=NOW_TIME;
+          $ar['fly_update_person']=$user[0]['true_name'];
+          $record=D('TreeFly');
+          $record->addData($ar);
+          $this->ajaxReturn($fly_tid);
+      }
+      else
+      {
+        $tree_id = I('get.tid');
+        $this->assign('tree_id',$tree_id);
+        $content=$this->fetch();
+        $this->ajaxReturn($content);
 
 
-    //   }   
+      }   
    }
 
 
