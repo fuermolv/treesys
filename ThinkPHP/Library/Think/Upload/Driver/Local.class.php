b81cedae280033d0d9cf0cfe0109fa1903fad705
fuermolv <302889601@qq.com>
2018-03-16 20:26:16 +0800

    ·
    1

--- ThinkPHP/Library/Think/Upload/Driver/Local.class.php
+++ ThinkPHP/Library/Think/Upload/Driver/Local.class.php
@@ -80,22 +80,23 @@ class Local
         $filename = $this->rootPath . $file['savepath'] . $file['savename'];
 
         /* 不覆盖同名文件 */
+
         if (!$replace && is_file($filename)) {
             $this->error = '存在同名文件' . $file['savename'];
             return false;
         }
 
         /* 移动文件 */
-        /*if (!move_uploaded_file($file['tmp_name'], $filename)) {
-            $this->error = '文件上传保存错误！';
-            return false;
-        }*/
-
-        if (!move_uploaded_file($file['tmp_name'], iconv('utf-8','gb2312',$filename)))  
-         {
+        if (!move_uploaded_file($file['tmp_name'], $filename)) {
             $this->error = '文件上传保存错误！';
             return false;
         }
+       
+        // if (!move_uploaded_file($file['tmp_name'], iconv('utf-8','gb2312',$filename)))  
+        //  {
+        //     $this->error = '文件上传保存错误！';
+        //     return false;
+        // }
 
         return true;
     }
